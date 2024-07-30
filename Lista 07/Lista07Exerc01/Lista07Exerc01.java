62a1b439b5436df9f43e8d58c02aeab60cfa8683
LUIZ CELIO GOMES FILHO <40032056+Domzera@users.noreply.github.com>
2024-07-30 17:52:34 -0300

    Adicionando a Lista 07
    Lista07

---
+++ Lista 07/Lista07Exerc01/Lista07Exerc01.java
@@ -0,0 +1,17 @@
+public class Lista07Exerc01 {
+	
+	public static void main (String[] args) {
+		int i;
+		
+		for(i=0;i<=255;i++){
+				
+				System.out.println(i+" = "+(char)i);
+		}
+		
+		for(i=35;i<=43;i++){
+				
+				System.out.println("\n"+i+" = "+(char)i);
+		}
+	}
+}
+
